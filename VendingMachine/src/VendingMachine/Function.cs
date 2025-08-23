ba9241b041591fb15d9e8b6f32db3fc450fb0f1e
Brandon Fulljames <bfullj@gmail.com>
2025-08-23 09:18:27 -0700

    Finally have working lambda API endpoint

--- VendingMachine/src/VendingMachine/Function.cs
+++ VendingMachine/src/VendingMachine/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -14,8 +15,22 @@ public class Function
     /// <param name="input">The event for the Lambda function handler to process.</param>
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest input, ILambdaContext context)
     {
-        return input.ToUpper();
+        var body = string.IsNullOrEmpty(input.Body) ? "ok" : input.Body;
+        if (input.IsBase64Encoded)
+        {
+            var bytes = Convert.FromBase64String(input.Body);
+            body = System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        var response = new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = 200,
+            Body = body.ToUpper() + "!",
+            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+        };
+
+        return response;
     }
 }

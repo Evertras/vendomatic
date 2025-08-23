f04e021578cb7fc0ae69a73018909ae6f22f2d25
Brandon Fulljames <bfullj@gmail.com>
2025-08-23 09:43:06 -0700

    Fix test

--- VendingMachine/test/VendingMachine.Tests/FunctionTest.cs
+++ VendingMachine/test/VendingMachine.Tests/FunctionTest.cs
@@ -13,8 +13,14 @@ public class FunctionTest
         // Invoke the lambda function and confirm the string was upper cased.
         var function = new Function();
         var context = new TestLambdaContext();
-        var upperCase = function.FunctionHandler("hello world", context);
+        var input = new Amazon.Lambda.APIGatewayEvents.APIGatewayHttpApiV2ProxyRequest
+        {
+            Body = "hello world",
+            IsBase64Encoded = false
+        };
 
-        Assert.Equal("HELLO WORLD", upperCase);
+        var result = function.FunctionHandler(input);
+
+        Assert.Equal("HELLO WORLD!", result.Body);
     }
 }

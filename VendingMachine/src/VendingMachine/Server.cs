fecef75d0c616e53327d544e9e240279dc33839f
Brandon Fulljames <bfullj@gmail.com>
2025-08-24 14:50:27 -0700

    Start to move req/res boilerplate out of handlers

--- VendingMachine/src/VendingMachine/Server.cs
+++ VendingMachine/src/VendingMachine/Server.cs
@@ -24,15 +24,56 @@ namespace VendingMachine
             // TODO: better logging
             Console.WriteLine(JsonSerializer.Serialize(input));
 
-            // Dumbest router ever, but for now it works... note this is keyed off AWS API Gateway integration entries,
-            // not the actual HTTP method and path
-            switch (input.RouteKey)
+            try
             {
-                case "POST /api/v1/machine":
-                    return await CreateMachine(input);
+                // Dumbest router ever, but for now it works... note this is keyed off AWS API Gateway integration entries,
+                // not the actual HTTP method and path
+                switch (input.RouteKey)
+                {
+                    case "POST /api/v1/machine":
+                        return await CreateMachine(input);
 
-                case "GET /api/v1/machine":
-                    return await ListMachines(input);
+                    case "GET /api/v1/machine":
+                        return await ListMachines(input);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Argument exception: {ex}");
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = ex.Message,
+                };
+            }
+            catch (NotSupportedException ex)
+            {
+                Console.WriteLine($"Not supported exception: {ex}");
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 415,
+                    Body = ex.Message,
+                };
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Json exception: {ex}");
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = ex.Message,
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unknown exception: {ex}");
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 500,
+                    // Temporary for quick debugging
+                    Body = ex.Message,
+                    // Body = "Internal Server Error",
+                };
             }
 
             return new APIGatewayHttpApiV2ProxyResponse
@@ -42,18 +83,20 @@ namespace VendingMachine
             };
         }
 
-        // Figure out a better place later, but for now just a cheaty spot for this
-        internal class CreateMachineRequest
+        internal static APIGatewayHttpApiV2ProxyResponse JsonResponse<T>(T obj, int statusCode = 200)
         {
-            public string? Name { get; set; }
+            return new APIGatewayHttpApiV2ProxyResponse
+            {
+                StatusCode = statusCode,
+                Body = JsonSerializer.Serialize(obj),
+                Headers = new Dictionary<string, string>
+                {
+                    { "Content-Type", "application/json" }
+                }
+            };
         }
 
-        internal class CreateMachineResponse
-        {
-            public string Id { get; set; } = string.Empty;
-        }
-
-        internal async Task<APIGatewayHttpApiV2ProxyResponse> CreateMachine(APIGatewayHttpApiV2ProxyRequest input)
+        internal static T ParseBodyRequest<T>(APIGatewayHttpApiV2ProxyRequest input)
         {
             if (string.IsNullOrEmpty(input.Body))
             {
@@ -68,9 +111,35 @@ namespace VendingMachine
                 body = Encoding.UTF8.GetString(bytes);
             }
 
-            // TODO: try/catch here for json fails
+            string contentType = "text/plain";
+            if (input.Headers.TryGetValue("content-type", out var headerContentType) && !string.IsNullOrEmpty(headerContentType))
+            {
+                contentType = headerContentType;
+            }
 
-            var req = JsonSerializer.Deserialize<CreateMachineRequest>(body);
+            switch (contentType)
+            {
+                case "application/json":
+                    return JsonSerializer.Deserialize<T>(body) ?? throw new ArgumentException("Failed to parse JSON body");
+                default:
+                    throw new NotSupportedException($"Unsupported Content-Type {contentType}");
+            }
+        }
+
+        // Figure out a better place later, but for now just a cheaty spot for this
+        internal class CreateMachineRequest
+        {
+            public string? Name { get; init; }
+        }
+
+        internal class CreateMachineResponse
+        {
+            public string Id { get; init; } = string.Empty;
+        }
+
+        internal async Task<APIGatewayHttpApiV2ProxyResponse> CreateMachine(APIGatewayHttpApiV2ProxyRequest input)
+        {
+            var req = ParseBodyRequest<CreateMachineRequest>(input);
 
             if (req == null || string.IsNullOrEmpty(req.Name))
             {
@@ -84,28 +153,14 @@ namespace VendingMachine
 
             var id = await _repository.AddMachineAsync(machine);
 
-            return new APIGatewayHttpApiV2ProxyResponse
-            {
-                StatusCode = 201,
-                Body = JsonSerializer.Serialize(new CreateMachineResponse { Id = id }),
-            };
+            return JsonResponse(new CreateMachineResponse { Id = id });
         }
 
         internal async Task<APIGatewayHttpApiV2ProxyResponse> ListMachines(APIGatewayHttpApiV2ProxyRequest input)
         {
             var machines = await _repository.ListMachinesAsync();
 
-            var body = JsonSerializer.Serialize(machines);
-
-            return new APIGatewayHttpApiV2ProxyResponse
-            {
-                StatusCode = 200,
-                Body = body,
-                Headers = new Dictionary<string, string>
-                {
-                    { "Content-Type", "application/json" }
-                }
-            };
+            return JsonResponse(machines);
         }
     }
 }

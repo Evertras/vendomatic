6f6f4a83f99c28238994b258a07e7e627036b958
Brandon Fulljames <bfullj@gmail.com>
2025-08-24 07:44:00 -0700

    Separate server logic from dependency creation

--- VendingMachine/src/VendingMachine/Function.cs
+++ VendingMachine/src/VendingMachine/Function.cs
@@ -8,50 +8,22 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace VendingMachine;
 
+// Acts as an entry point for AWS to invoke the Lambda function. Only worry about
+// setting up dependencies and passing the request to the Server where the logic is.
 public class Function
 {
-    readonly Repository _repository;
+    readonly Server _server;
 
     public Function()
     {
         var db = new AmazonDynamoDBClient();
         var tableName = Environment.GetEnvironmentVariable("EVERTRAS_TABLE_NAME") ?? "evertras-vendomatic-db";
-        _repository = new Repository(db, tableName);
+        var repository = new Repository(db, tableName);
+        _server = new Server(repository);
     }
     
     public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest input)
     {
-        var body = string.IsNullOrEmpty(input.Body) ? "ok" : input.Body;
-        if (input.IsBase64Encoded)
-        {
-            var bytes = Convert.FromBase64String(input.Body);
-            body = System.Text.Encoding.UTF8.GetString(bytes);
-        }
-
-        if (string.IsNullOrEmpty(body))
-        {
-            var badResponse = new APIGatewayHttpApiV2ProxyResponse
-            {
-                StatusCode = 400,
-                Body = "Bad Request: No body",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
-            };
-            return badResponse;
-        }
-
-        var machine = new Models.Machine {
-            Name = body
-        };
-
-        await _repository.AddMachineAsync(machine);
-
-        var response = new APIGatewayHttpApiV2ProxyResponse
-        {
-            StatusCode = 200,
-            Body = "Created!",
-            Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
-        };
-
-        return response;
+        return await _server.HandlRequest(input);
     }
 }
---
+++ VendingMachine/src/VendingMachine/Server.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    // There's probably a million better ways to do this, but for now...
+    internal class Server
+    {
+        readonly IRepository _repository;
+
+        public Server(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<APIGatewayHttpApiV2ProxyResponse> HandlRequest(APIGatewayHttpApiV2ProxyRequest input)
+        {
+            var body = string.IsNullOrEmpty(input.Body) ? "ok" : input.Body;
+            if (input.IsBase64Encoded)
+            {
+                var bytes = Convert.FromBase64String(input.Body);
+                body = Encoding.UTF8.GetString(bytes);
+            }
+            if (string.IsNullOrEmpty(body))
+            {
+                var badResponse = new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = "Bad Request: No body",
+                    Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                };
+                return badResponse;
+            }
+            var machine = new Models.Machine
+            {
+                Name = body
+            };
+            await _repository.AddMachineAsync(machine);
+            var response = new APIGatewayHttpApiV2ProxyResponse
+            {
+                StatusCode = 200,
+                Body = "Created!",
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+            };
+            return response;
+        }
+    }
+}

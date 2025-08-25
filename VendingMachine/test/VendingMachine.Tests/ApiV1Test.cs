629c93098b83cb68d323ff050f46a5f4e9ad3175
Brandon Fulljames <bfullj@gmail.com>
2025-08-25 06:39:35 -0700

    Quick test for create

--- VendingMachine/test/VendingMachine.Tests/ApiV1Test.cs
+++ VendingMachine/test/VendingMachine.Tests/ApiV1Test.cs
@@ -1,12 +1,9 @@
 using Xunit;
 using NSubstitute;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.TestUtilities;
-using System.Collections.Generic;
 using System.Text.Json;
-using VendingMachine.Models;
 using VendingMachine.Dtos;
 using Amazon.DynamoDBv2;
+using Amazon.Lambda.APIGatewayEvents;
 
 namespace VendingMachine.Tests;
 
@@ -17,7 +14,7 @@ public class ApiV1Test
     {
         var mockAmazonDB = Substitute.For<IAmazonDynamoDB>();
 
-        mockAmazonDB.ScanAsync(Arg.Any<Amazon.DynamoDBv2.Model.ScanRequest>(), Arg.Any<System.Threading.CancellationToken>())
+        mockAmazonDB.ScanAsync(Arg.Any<Amazon.DynamoDBv2.Model.ScanRequest>(), Arg.Any<CancellationToken>())
             .Returns(new Amazon.DynamoDBv2.Model.ScanResponse
             {
                 Items =
@@ -27,14 +24,14 @@ public class ApiV1Test
                         { "SK", new Amazon.DynamoDBv2.Model.AttributeValue { S = "MAC#1234" } },
                         { "Name", new Amazon.DynamoDBv2.Model.AttributeValue { S = "Test Machine" } },
                         { "ExtraField", new Amazon.DynamoDBv2.Model.AttributeValue { S = "Shouldn't bother anyone" } },
-                        { "CreatedAt", new Amazon.DynamoDBv2.Model.AttributeValue { S = System.DateTime.UtcNow.ToString("o") } },
+                        { "CreatedAt", new Amazon.DynamoDBv2.Model.AttributeValue { S = DateTime.UtcNow.ToString("o") } },
                     }
                 ]
             });
 
         var server = new Server(new Repository(mockAmazonDB, "test-table"));
 
-        var res = await server.HandleRequest(new Amazon.Lambda.APIGatewayEvents.APIGatewayHttpApiV2ProxyRequest
+        var res = await server.HandleRequest(new APIGatewayHttpApiV2ProxyRequest
         {
             RouteKey = "GET /api/v1/machines",
         });
@@ -43,7 +40,7 @@ public class ApiV1Test
         {
             Machines =
             [
-                new Dtos.Machine
+                new Machine
                 {
                     Id = "1234",
                     Name = "Test Machine",
@@ -56,5 +53,37 @@ public class ApiV1Test
         Assert.Equal(expectedBody, res.Body);
         Assert.True(res.Headers.ContainsKey("Content-Type"));
         Assert.Equal("application/json", res.Headers["Content-Type"]);
+    }
+
+    [Fact]
+    public async Task TestCreateMachineCreatesMachine()
+    {
+        var mockAmazonDB = Substitute.For<IAmazonDynamoDB>();
+        mockAmazonDB.PutItemAsync(Arg.Any<Amazon.DynamoDBv2.Model.PutItemRequest>(), Arg.Any<CancellationToken>())
+            .Returns(new Amazon.DynamoDBv2.Model.PutItemResponse());
+        var server = new Server(new Repository(mockAmazonDB, "test-table"));
+        var req = new MachineCreateRequest
+        {
+            Name = "New Machine",
+        };
+        var reqBody = JsonSerializer.Serialize(req);
+        var reqBodyBase64 = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(reqBody));
+        var res = await server.HandleRequest(new APIGatewayHttpApiV2ProxyRequest
+        {
+            RouteKey = "POST /api/v1/machines",
+            Body = reqBodyBase64,
+            IsBase64Encoded = true,
+            Headers = new Dictionary<string, string>
+            {
+                { "content-type", "application/json" }
+            }
+        });
+        Assert.Equal(200, res.StatusCode);
+        Assert.NotNull(res.Body);
+        Assert.True(res.Headers.ContainsKey("Content-Type"));
+        Assert.Equal("application/json", res.Headers["Content-Type"]);
+        var resObj = JsonSerializer.Deserialize<MachineCreateResponse>(res.Body!);
+        Assert.NotNull(resObj);
+        Assert.False(string.IsNullOrEmpty(resObj.Machine.Id));
     }
 }

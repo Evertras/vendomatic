7f7ab045b6259f118de464cedab8679a7c4ea3be
Brandon Fulljames <bfullj@gmail.com>
2025-08-24 18:25:27 -0700

    Formatting

--- terraform/modules/dotnet_lambda/tracing.tf
+++ terraform/modules/dotnet_lambda/tracing.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_policy" "tracing_policy" {
-    name        = "${local.prefix}-tracing-policy"
-    description = "Policy for X-Ray tracing"
-    policy      = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-            {
-                Effect = "Allow"
-                Action = [
-                    "xray:PutTelemetryRecords",
-                    "xray:PutTraceSegments"
-                ]
-                Resource = "*"
-            }
+  name        = "${local.prefix}-tracing-policy"
+  description = "Policy for X-Ray tracing"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "xray:PutTelemetryRecords",
+          "xray:PutTraceSegments"
         ]
-    })
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_tracing_policy" {
-    role       = aws_iam_role.lambda_exec.name
-    policy_arn = aws_iam_policy.tracing_policy.arn
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.tracing_policy.arn
 }
